f270fbc531503ebda9a286bd9ce572db31bdad61
Ryan Freeman <info@ryanfreeman.io>
2019-11-05 21:57:32 +0000

    Added routes for OAuth2 redirect and callback

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/redirect', function () {
+
+    $query = http_build_query([
+        'client_id' => '3',
+        'redirect_uri' => 'http://client-server.loc/callback',
+        'response_type' => 'code',
+        'scope' => ''
+    ]);
+
+    return redirect('http://community-poll.loc/oauth/authorize?'.$query);
+});
+
+Route::get('/callback', function (Illuminate\Http\Request $request) {
+    $http = new \GuzzleHttp\Client;
+
+    $response = $http->post('http://community-poll.loc/oauth/token', [
+        'form_params' => [
+            'client_id' => '3',
+            'client_secret' => 'm7Pd2ZSBWIMZNgIKvNPr4zOD1FMCXdAu9O35bINb',
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => 'http://client-server.loc/callback',
+            'code' => $request->code,
+        ],
+    ]);
+    return json_decode((string) $response->getBody(), true);
+});
